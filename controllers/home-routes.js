8a5415442650873f6e128d8e3d830b3d7e3c42e3
Parker <parkerriddle09@gmail.com>
2022-09-26 21:18:13 -0600

    Added links for the artists, songs, and albums in the nav bar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,19 +4,43 @@ const { Gallery, Painting } = require('../models');
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   try {
-    // const dbGalleryData = await Gallery.findAll({
-    //   include: [
-    //     {
-    //       model: Painting,
-    //       attributes: ['filename', 'description'],
-    //     },
-    //   ],
-    // });
-
-    // const galleries = dbGalleryData.map((gallery) =>
-    //   gallery.get({ plain: true })
-    // );
     res.render('homepage', {
+      // galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/artist', async (req, res) => {
+  try {
+    res.render('artist', {
+      // galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/album', async (req, res) => {
+  try {
+    res.render('album', {
+      // galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/song', async (req, res) => {
+  try {
+    res.render('song', {
       // galleries,
       loggedIn: req.session.loggedIn,
     });
