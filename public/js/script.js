5fa7ef2022530a4f4ca8df529757ef06f6c888d0
DylanMacres <Dylan.Macres@du.edu>
2022-09-29 21:10:39 -0600

    adding more api

--- public/js/script.js
+++ public/js/script.js
@@ -25,6 +25,20 @@ function getArtist (artistSearch) {
     };
 
 
+    function searchArtist (artistSearch){
+        let requestUrl = "https://theaudiodb.com/api/v1/json/2/artist.php?i=" + artistSearch;
+        fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data){
+            artistName = data.artists[x].strArtist;
+            getGenre = data.artists[x].strGenre;
+            console.log(artistName);
+            console.log(getGenre);
+        })
+
+    }
+
 
     function albums (artistSearch){
         let requestUrl = "https://theaudiodb.com/api/v1/json/523532/searchalbum.php?s=" + artistSearch;
@@ -32,6 +46,10 @@ function getArtist (artistSearch) {
         fetch(requestUrl)
         .then(function (response){
             return response.json();
+        }).then (function(data){
+            albumName = data.album[x].strAlbum;
+        
+
         })
     }
     
