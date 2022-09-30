ce83efd08b4673a4fd61701af4bdf46dd894cac6
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-09-29 21:38:14 -0500

    artist model created under model folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,3 @@
-DROP DATABASE IF EXISTS usermusic_db;
+DROP DATABASE IF EXISTS userMusic_db;
 
-CREATE DATABASE usermsuic_db;
-
-USE usermusic_db;
-
---creates table for the users music on the dashboard--
-CREATE TABLE music (
-    id INT NOT NULL,
-    songs VARCHAR(100) NOT NULL,
-    artists VARCHAR(100) NOT NULL,
-    albums VARCHAR(100) NOT NULL,
-);
+CREATE DATABASE userMusic_db;
