477b58f45ddfe759707e6f7680554fecbdff587d
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-10-13 19:14:41 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -38,10 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(
-      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-    )
-  );
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
+
